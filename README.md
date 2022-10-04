08cc8a3a9ee12c2aa4dfe451adfa8328c8de6e7c
Gonzalo <gbd1004@alu.ubu.es>
2022-10-05 01:04:46 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Edgerunners_dotfiles WIP
---
+++ nvim/init.lua
@@ -0,0 +1,94 @@
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.bo.autoindent = true
+vim.smarttab = true
+vim.mouse = true
+
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+Plug 'https://github.com/Mofiqul/dracula.nvim'
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = 'TSUpdate'})
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug('neoclide/coc.nvim', {['branch'] = 'release'})
+Plug 'scrooloose/nerdtree'
+Plug 'jiangmiao/auto-pairs'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'lifepillar/vim-colortemplate'
+
+vim.call('plug#end')
+
+require('telescope_keys')
+
+require'nvim-treesitter.configs'.setup {
+  -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "maintained",
+
+  -- Install languages synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- List of parsers to ignore installing
+  ignore_install = { "javascript" },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- list of language that will be disabled
+    --disable = { "c", "rust" },
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+vim.cmd[[source /home/gonzalo/.config/nvim/coc.vim]]
+
+vim.cmd[[nnoremap <silent> <C-k><C-B> :NERDTreeToggle<CR>]]
+
+-- customize dracula color palette
+vim.g.dracula_colors = {
+  bg = "#0D1F2D",
+  fg = "#EAF0FC",
+  selection = "#EC4C5E",
+  comment = "#6272A4",
+  red = "#F00038",
+  orange = "#F74E75",
+  yellow = "#F6F14F",
+  green = "#5DFF10",
+  purple = "#5645AA",
+  cyan = "#00FFFF",
+  pink = "#FF10CA",
+  bright_red = "#F1315E",
+  bright_green = "#7DFF3E",
+  bright_yellow = "#FFFB74",
+  bright_blue = "#7465C0",
+  bright_magenta = "#FF3ED4",
+  bright_cyan = "#60FDFD",
+  bright_white = "#EAF0FC",
+  menu = "#21222C",
+  visual = "#3E4452",
+  gutter_fg = "#4B5263",
+  nontext = "#3B4048",
+  white = "#C5D3EF",
+  black = "#282A2E",
+}
+-- show the '~' characters after the end of buffers
+vim.g.dracula_show_end_of_buffer = true
+-- use transparent background
+vim.g.dracula_transparent_bg = true
+-- set custom lualine background color
+vim.g.dracula_lualine_bg_color = "#44475a"
+-- set italic comment
+vim.g.dracula_italic_comment = true
+
+vim.cmd[[colorscheme dracula]]
